c1bbaa4999884621c97533239a31e1ae6ded2dde
TWR82Q <thax.don@yahoo.com>
2023-02-03 05:19:05 +0700

    bentuk pertama

--- js/main.js
+++ js/main.js
@@ -13,5 +13,20 @@ var swiper2 = new Swiper(".bg-slider", {
 });
 
 //Navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
 
 //Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn")
+const closeBtn = document.querySelector(".nav-close-btn")
+const navigation = document.querySelector(".navigation")
+
+menuBtn.addEventListener("click", () =>{
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () =>{
+    navigation.classList.remove("active");
+});
